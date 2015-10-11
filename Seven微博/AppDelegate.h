130e8ad8c8f511caa0ac21a75f5e0d4d3d782085
mac <mac@macdeMacBook-Air-6.local>
2015-10-11 15:20:05 +0800

    Initial Commit

---
+++ Seven微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Seven微博
+//
+//  Created by mac on 15/10/11.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Seven微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seven微博
+//
+//  Created by mac on 15/10/11.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
